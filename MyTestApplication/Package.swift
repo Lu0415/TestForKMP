373d0468d6280c04a5ad519bd4fa078277a46720
Lu0415 <cyril0415@gmail.com>
2025-01-09 16:50:08 +0800

    Update Package.swift targets url
    更新目標連結網址

--- MyTestApplication/Package.swift
+++ MyTestApplication/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "",
+            url: "https://github.com/Lu0415/TestForKMP/releases/tag/1.0.0#:~:text=Shared.xcframework.zip",
             checksum: "0529daea00462d986c6b4f1d8c83dcdc3134e101b1fb153953254e6b64588994"
         )
     ]
