cac44794bc4735e9d8a6e72c50c5d7d6f8d4f3ea
Lu0415 <cyril0415@gmail.com>
2025-01-09 17:36:08 +0800

    Update Package.swift with shared.xcframework

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "https://github.com/Lu0415/TestForKMP/releases/tag/1.0.0#:~:text=Shared.xcframework.zip",
-            checksum: "0529daea00462d986c6b4f1d8c83dcdc3134e101b1fb153953254e6b64588994"
+            url: "https://github.com/Lu0415/TestForKMP/releases/tag/1.0.0#:~:text=shared.xcframework.zip",
+            checksum: "7442e368a0ad36936bedfc36b57a8ce2140318dfec89344d5183d14bc09cc673"
         )
     ]
 )
