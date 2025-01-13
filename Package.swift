8ca19aab98ed6a38eea67f384a6a668120885298
Lu0415 <cyril0415@gmail.com>
2025-01-13 08:43:28 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            url: "https://github.com/Lu0415/TestForKMP/releases/tag/1.0.0#:~:text=shared.xcframework.zip",
+            url: "https://github.com/Lu0415/TestForKMP/releases/tag/1.0.3#:~:text=shared.xcframework.zip",
             checksum: "7442e368a0ad36936bedfc36b57a8ce2140318dfec89344d5183d14bc09cc673"
         )
     ]
