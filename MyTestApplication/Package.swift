1b775384ee8703e1be18e80224aacd4c6a11c404
Lu0415 <cyril0415@gmail.com>
2025-01-13 08:43:01 +0800

    Update Package.swift

--- MyTestApplication/Package.swift
+++ MyTestApplication/Package.swift
@@ -7,12 +7,12 @@ let package = Package(
         .iOS(.v14),
     ],
     products: [
-        .library(name: "Shared", targets: ["Shared"])
+        .library(name: "shared", targets: ["shared"])
     ],
     targets: [
         .binaryTarget(
-            name: "Shared",
-            url: "https://github.com/Lu0415/TestForKMP/releases/tag/1.0.0#:~:text=Shared.xcframework.zip",
+            name: "shared",
+            url: "https://github.com/Lu0415/TestForKMP/releases/tag/1.0.3#:~:text=shared.xcframework.zip",
             checksum: "0529daea00462d986c6b4f1d8c83dcdc3134e101b1fb153953254e6b64588994"
         )
     ]
